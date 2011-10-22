f6e1d3cd4ced1a4c551791af6191bae9e7b049fb
Phil A <maemo@smurfy.de>
2011-10-22 22:08:24 +0200

    Some Updates trying to compile on obs

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
 #include <QtGui/QApplication>
 #include <qplatformdefs.h> // MEEGO_EDITION_HARMATTAN
 
-#if defined(MEEGO_EDITION_HARMATTAN) || defined(Q_WS_MAEMO_5)
+#if defined(MEEGO_VERSION_MAJOR) || defined(Q_WS_MAEMO_5)
     #include <QtDeclarative>
 #elif defined(Q_WS_WIN) || defined(Q_WS_X11)
     #include "gui/desktop-test/mainwindow.h"
@@ -17,7 +17,7 @@ int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
 
-    #if defined(MEEGO_EDITION_HARMATTAN) || defined(Q_WS_MAEMO_5)
+    #if defined(MEEGO_VERSION_MAJOR) || defined(Q_WS_MAEMO_5)
         qmlRegisterType<Fahrplan>("Fahrplan", 1, 0, "Backend");
         qmlRegisterType<ParserAbstract>("Fahrplan", 1, 0, "ParserAbstract");
         qmlRegisterType<FahrplanFavoritesManager>("Fahrplan", 1, 0, "FahrplanFavoritesManager");
@@ -29,7 +29,7 @@ int main(int argc, char *argv[])
         qmlRegisterType<JourneyDetailResultItem>("Fahrplan", 1, 0, "JourneyDetailResultItem");
 
         QDeclarativeView view;
-        #if defined(MEEGO_EDITION_HARMATTAN)
+        #if defined(MEEGO_VERSION_MAJOR)
             view.setSource(QUrl("qrc:/src/gui/harmattan/main.qml"));
             view.showFullScreen();
         #endif
