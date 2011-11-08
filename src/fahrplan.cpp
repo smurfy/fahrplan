27dfbd8e61771255f5123daa6648d0653e598be4
Phil. A <maemo@smurfy.de>
2011-11-08 21:43:58 +0100

    Updating version informations

--- src/fahrplan.cpp
+++ src/fahrplan.cpp
@@ -53,7 +53,7 @@ FahrplanFavoritesManager* Fahrplan::favorites()
 
 QString Fahrplan::getVersion()
 {
-    return "2.0.2 Alpha";
+    return "2.0.3 Alpha";
 }
 
 void Fahrplan::onFavoritesChanged(QStringList favorites)
