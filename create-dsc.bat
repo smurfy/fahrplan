fe0d304f0fb3d7cd4f396ae237148a40fc576721
Phil. A <maemo@smurfy.de>
2013-05-24 10:52:48 +0200

    Adding tmp dir to dsc ignore list too

--- create-dsc.bat
+++ create-dsc.bat
@@ -1,1 +1,1 @@
-C:/QTSDK/Madde/wbin/mad.cmd -t harmattan_10.2011.34-1_rt1.2 dpkg-buildpackage -sa -S -uc -us -Imoc -Iobj -Ircc -Iui -I.svn -I.git -I*.deb -I*.changes -Iqtc_packaging -IMakefile -I*.pro.user -Idebug -Irelease -I*release -I*debug -Ifahrplan2
+C:/QTSDK/Madde/wbin/mad.cmd -t harmattan_10.2011.34-1_rt1.2 dpkg-buildpackage -sa -S -uc -us -Imoc -Iobj -Ircc -Iui -I.svn -I.git -I*.deb -I*.changes -Iqtc_packaging -IMakefile -I*.pro.user -Idebug -Irelease -I*release -I*debug -Ifahrplan2 -Itmp/*
