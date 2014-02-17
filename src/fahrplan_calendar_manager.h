16c806e0d2f52eb51aaf1523d993212d4f56bceb
Philipp Andreas <github@smurfy.de>
2014-02-17 18:39:57 +0100

    Fix for compile error

--- src/fahrplan_calendar_manager.h
+++ src/fahrplan_calendar_manager.h
@@ -22,7 +22,7 @@
 
 #include <QAbstractListModel>
 
-#if !defined(BUILD_FOR_BLACKBERRY) && !defined(BUILD_FOR_SAILFISHOS)
+#if !defined(BUILD_FOR_BLACKBERRY) && !defined(BUILD_FOR_SAILFISHOS) && !defined(BUILD_FOR_DESKTOP)
 #   include <QOrganizerCollectionId>
 QTM_USE_NAMESPACE
 #endif
