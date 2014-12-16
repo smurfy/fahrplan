05f43eb0142fe35ef0df0a9d885731afadf73dad
Philipp Andreas <github@smurfy.de>
2014-12-16 18:34:52 +0100

    Removed old about text

--- src/gui/about.js
+++ src/gui/about.js
@@ -19,22 +19,6 @@
 
 .pragma library
 
-var aboutText = "<p>This or prior versions uses code contribution by:<br><i>gri</i>, <i>lorenzph</i>, <i>CaCO3</i>, <i>hcm</i>, <i>thp</i>, <i>qwerty12</i>, <i>qbast</i>, <i>Thomas Fischer</i>, <i>Oleksii Serdiuk</i>, <i>NightShift79</i>, <i>pirast</i>, <i>mzanetti</i>, <i>shentok</i>, <i>evansebire</i>, <i>unsanded</i>, <i>Morpog</i></p>" +
-        "<p>Fahrplan contains the following contributed translations: German by <i>smurfy</i> and <i>Florian Wittmann</i>, Ukrainian by <i>Oleksii Serdiuk</i>, Russian by <i>Oleksii Serdiuk</i> and <i>Alexey (lewa)</i>, Romanian by <i>LaurB (laurbb)</i>, Dutch by <i>Heimen Stoffels (Vistaus)</i>, Persian by <i>Ali Adineh (ali6007205)</i>, Polish by <i>maurycy</i>.<br><a href=\"https://www.transifex.com/projects/p/fahrplan/\">Translate to your language</a></p>" +
-        "<p>If you like fahrplan, i would appreciate a donation.<br><a href=\"https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=DW3LGJ6VEKFVS\">Donate using PayPal</a></p>" +
-        "<p>If you encounter bugs or have feature requests visit the <br><a href=\"https://github.com/smurfy/fahrplan/issues\">Fahrplan issue tracker on GitHub</a></p>" +
-        "<p><b>License</b></p>" +
-        "<p>This program is free software; you can redistribute it and/or " +
-        "modify it under the terms of the GNU General Public License " +
-        "as published by the Free Software Foundation; either version 2 " +
-        "of the License, or (at your option) any later version.</p> " +
-        "<p>This program is distributed in the hope that it will be useful, " +
-        "but WITHOUT ANY WARRANTY; without even the implied warranty of " +
-        "MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the " +
-        "GNU General Public License for more details.</p> " +
-        "<p>You should have received a copy of the GNU General Public License " +
-        "along with this program. If not, see &lt;<a href=\"http://www.gnu.org/licenses/\">http://www.gnu.org/licenses/</a>&gt;.</p>";
-
 var copyright = "(C) 2014 smurfy (maemo@smurfy.de)";
 
 var license = "<p>This program is free software; you can redistribute it and/or " +
