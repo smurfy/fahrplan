f3a72eb417d2da29f4c2c57a71815023520f2aa8
Chris Clime <chris.clime@gmx.net>
2018-01-07 13:10:08 +0100

    merge for pull request

--- src/gui/about.js
+++ src/gui/about.js
@@ -37,7 +37,8 @@ var support = qsTr("<p>If fahrplan is not translated in your language, help us <
         "<p>If you encounter bugs or have feature requests visit the <a href=\"https://github.com/smurfy/fahrplan/issues\">Fahrplan issue tracker on GitHub</a>.</p>");
 
 var maintainers = ["Philipp Andreas (smurfy)", "Michael Stevens (stevensmi)", "Oleksii Serdiuk (leppa)"];
-var codeContributors = ["gri", "lorenzph", "CaCO3", "hcm", "thp", "qwerty12", "qbast", "Thomas Fischer", "Oleksii Serdiuk (leppa)", "Nekhelesh Ramananthan (nik90)", "NightShift79", "pirast", "Michael Zanetti (mzanetti)", "shentok", "evansebire", "unsanded", "Morpog", "benni0815", "Erik Lundin", "balcy"].sort();
+var codeContributors = ["gri", "lorenzph", "CaCO3", "hcm", "thp", "qwerty12", "qbast", "Thomas Fischer", "Oleksii Serdiuk (leppa)", "Nekhelesh Ramananthan (nik90)", "NightShift79", "pirast", "Michael Zanetti (mzanetti)", "Chris Clime (balcy)", "dayrover", "shentok", "evansebire", "unsanded", "Morpog", "benni0815", "Erik Lundin"].sort();
+
 var translators = [
             "Arabic - Sary AWAD (sary.1991)",
             "Catalan - Felip Martí (FelipMarti)",
